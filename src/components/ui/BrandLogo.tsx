1cbed7de9bbe24407bc5431d6fa20d620378595d
Roshin RG <roshin.rg.2024.aids@rajalakshmi.edu.in>
2026-09-17 14:35:37 +0530

    Use Vikram.png as navbar logo

--- src/components/ui/BrandLogo.tsx
+++ src/components/ui/BrandLogo.tsx
@@ -22,7 +22,7 @@ export function BrandLogo({
 }: BrandLogoProps) {
   const image = fillWidth ? (
     <Image
-      src="/images/TeamVikram.png"
+      src="/images/Vikram.png"
       alt="Team Vikram"
       width={1024}
       height={1024}
@@ -35,7 +35,7 @@ export function BrandLogo({
     />
   ) : (
     <Image
-      src="/images/TeamVikram.png"
+      src="/images/Vikram.png"
       alt="Team Vikram"
       width={1024}
       height={1024}
